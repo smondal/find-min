790d1c21cfd8c67d588e54d5c4a0490173e6a303
sandip mondal <smondal2@wsgc.com>
2019-09-15 09:57:57 +0530

    Fix formatting

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 /* eslint no-extend-native: ["error", { "exceptions": ["Array"] }] */
 const flatten = function (arr) {
 	return arr.reduce((flat, toFlatten) => {
-		return flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten);
+		return flat.concat(
+			Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten
+		);
 	}, []);
 };
 
@@ -14,9 +16,9 @@ module.exports = (...obj) => {
 	if (Array.isArray(...obj)) {
 		const flattenarr = flatten(...obj);
 		min = Math.min.apply(null, flattenarr);
-	}	else if (typeof obj === 'object' && Object.keys(...obj).length >= 1) {
-		min = Object.keys(...obj).reduce((a, b) => obj[a] > obj[b] ? b : a);
-	}	else {
+	} else if (typeof obj === 'object' && Object.keys(...obj).length >= 1) {
+		min = Object.keys(...obj).reduce((a, b) => (obj[a] > obj[b] ? b : a));
+	} else {
 		min = Math.min(...obj);
 	}
 
